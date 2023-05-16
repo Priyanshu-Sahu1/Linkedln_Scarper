1d063ee261065df307e05691359147a916abfab6
Priyanshu-Sahu1 <priyanshu.sahu@iiitg.ac.in>
2023-05-16 12:25:59 +0530

    Update parameter.py

--- LinkedlnProfile_URLDataScraper/parameter.py
+++ LinkedlnProfile_URLDataScraper/parameter.py
@@ -4,6 +4,7 @@ Linkedln_password = 'Enter the password'
 Url='https://www.google.com'
 #SearchQuery : on the basis which we need to scrape data
 Query= 'site:linkedin.com/in/ AND'+'"Enter City Name"'
+#Change only the city name for what location you searching the linkedln user profiles
 #Enter the relative path of your chromedriver
 relative_path = "./chromedriver.exe"
 #Enter number of Urls you want to extract.
